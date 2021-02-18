e7d6c6c0e5eff9ea92d5c259426abc3d08303573
yousif-loqman <79228538+yousif-loqman@users.noreply.github.com>
2021-02-18 15:02:44 +0300

    Add files via upload

---
+++ java/camper.js
@@ -0,0 +1,11 @@
+let mimati = document.getElementById("xxx")
+ 
+
+ function sum(){
+    let f=document.getElementById("in1").value
+    let d = Number (f)
+ let c =  d + 10
+mimati.innerHTML=c
+   
+}
+
